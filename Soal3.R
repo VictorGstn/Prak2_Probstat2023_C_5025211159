8a1b2ec7d2eb44dba5961c690d766ef4b2e93010
VictorGstn <125529445+VictorGstn@users.noreply.github.com>
2023-06-02 11:47:16 +0700

    Update Soal3.R

--- Soal3.R
+++ Soal3.R
@@ -1,10 +1,8 @@
 library(BSDA)
-hasil<-tsum.test(mean.x=3.64, s.x = 1.67, n.x =20 , 
+tsum.test(mean.x=3.64, s.x = 1.67, n.x =20 , 
           mean.y =2.79 , s.y = 1.5,
-          n.y = 27, alternative = "greater", 
-          mu = 0, var.equal = TRUE,
+          n.y = 27, alternative =  "two.sided", 
+           var.equal = TRUE,
           conf.level = 0.95)
-
-hasil$statistic
 
 qt(p=.025/2, df=2, lower.tail=FALSE)
