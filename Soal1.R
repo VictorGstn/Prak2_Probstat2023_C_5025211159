120630c82d099cefa47664e886631a66cce17807
VictorGstn <125529445+VictorGstn@users.noreply.github.com>
2023-06-02 11:45:50 +0700

    Update Soal1.R

--- Soal1.R
+++ Soal1.R
@@ -1,9 +1,7 @@
-data = data.frame(Responden=c(1:9),
+data1 = data.frame(Responden=c(1:9),
                   x =c(78,75,67,77,70,72,78,70,77),
                   y = c(100,95,70,90,90,90,89,100,100))
-print(data)
-dif= data$y - data$x
-print(dif)
+dif= data1$y - data1$x
 sd(dif)
 
 library(BSDA)
